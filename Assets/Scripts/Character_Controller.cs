0c953fcd9fdf9eba2a31e36d7a26e2b7ca491304
Casidar <casidar5@gmail.com>
2026-01-30 22:57:29 -0800

    Created basic wasd movement

--- Assets/Scripts/Character_Controller.cs
+++ Assets/Scripts/Character_Controller.cs
@@ -1,8 +1,22 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class Character_Controller : MonoBehaviour
 {
+    private PlayerInputActions playerControls;
+    private Rigidbody2D rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        playerControls = new PlayerInputActions();
+    }
+
+    private void OnEnable() {
+        playerControls.Player.Enable();
+    }
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,6 +26,11 @@ public class Character_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 movement = playerControls.Player.Move.ReadValue<Vector2>();
+        rb.linearVelocity = movement;
+    }
+
+    private void OnDisable() {
+        playerControls.Player.Disable();
     }
 }
